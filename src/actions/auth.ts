6ba7e6f8348e1879cbdcd207e739b3dca4d677e3
parthwebdev <parth.webdev@gmail.com>
2024-02-18 12:55:14 +0530

    Update actions in auth.ts

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,9 +1,11 @@
 "use server";
 
-import { formSchema, signupSchema } from "@/lib/types";
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { z } from "zod";
+
+import { formSchema } from "@/lib/types";
 
 export const logInUser = async ({
   email,
@@ -11,7 +13,7 @@ export const logInUser = async ({
 }: z.infer<typeof formSchema>) => {
   const supabase = createRouteHandlerClient({ cookies });
 
-  const response = supabase.auth.signInWithPassword({
+  const response = await supabase.auth.signInWithPassword({
     email,
     password,
   });
@@ -25,16 +27,11 @@ export const signUpUser = async ({
 }: z.infer<typeof formSchema>) => {
   const supabase = createRouteHandlerClient({ cookies });
 
-  // const { data } = supabase.from("profiles").select("*").eq("email", email);
-  // if (data?.length) return { error: {
-  //   message: 'User already exists'
-  // } };
-
-  const response = supabase.auth.signUp({
+  const response = await supabase.auth.signUp({
     email,
     password,
     options: {
-      emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}api/auth/callback`,
+      emailRedirectTo: "http://localhost:3000/api/auth/callback",
     },
   });
 
