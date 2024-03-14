8d1caeb569dafd406c93af3ad4a294f43a290820
parthwebdev <parth.webdev@gmail.com>
2024-03-14 15:10:35 +0530

    Add workspace schema

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -1,4 +1,24 @@
 import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { users } from "../../../migrations/schema";
+
+export const workspaces = pgTable("workspaces", {
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  title: text("title").notNull(),
+  iconId: text("icon_id").notNull(),
+  data: text("data"),
+  inTrash: text("in_trash"),
+  logo: text("logo"),
+  bannerUrl: text("banner_url"),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "string",
+  })
+    .defaultNow()
+    .notNull(),
+  workspaceOwner: uuid("workspace_owner")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+});
 
 export const folders = pgTable("folders", {
   id: uuid("id").defaultRandom().primaryKey().notNull(),
@@ -12,6 +32,11 @@ export const folders = pgTable("folders", {
   })
     .defaultNow()
     .notNull(),
+  workspaceId: uuid("workspace_id")
+    .notNull()
+    .references(() => workspaces.id, {
+      onDelete: "cascade",
+    }),
 });
 
 export const files = pgTable("files", {
