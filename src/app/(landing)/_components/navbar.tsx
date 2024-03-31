069f19e499f47190a988fad1f4e73fc5901380ad
parthwebdev <parth.webdev@gmail.com>
2024-03-31 14:21:38 +0530

    Add: fallback avatar image

--- src/app/(landing)/_components/navbar.tsx
+++ src/app/(landing)/_components/navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { cookies } from "next/headers";
+import Image from "next/image";
 
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { ArrowRight } from "lucide-react";
@@ -9,12 +10,16 @@ import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
+import avatar from "../../../../public/avatar.webp";
+
 async function Navbar() {
   const supabase = createServerComponentClient({ cookies });
 
   const {
     data: { user },
   } = await supabase.auth.getUser();
+
+  console.log(user);
 
   return (
     <nav className="px-10 py-6 flex justify-between items-center">
@@ -25,7 +30,9 @@ async function Navbar() {
           <>
             <Avatar className="w-8 h-8">
               <AvatarImage src={user.user_metadata.avatar_url} />
-              <AvatarFallback>PP</AvatarFallback>
+              <AvatarFallback>
+                <Image src={avatar} alt="Avatar" />
+              </AvatarFallback>
             </Avatar>
             <Button size="sm" className="flex gap-1">
               <Link href="/dashboard">Enter Notion</Link>
